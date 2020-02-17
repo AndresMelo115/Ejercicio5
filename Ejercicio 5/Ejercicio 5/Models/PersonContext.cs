efb63fca724414bf4576b5c3f2522cc933ae1233
Andres Melo <andresm121@hotmail.com>
2020-02-16 23:56:48 -0400

    Ejercicio5 -estado falto, existe pero no esta manejado con default en creacion ni alternacia con checkbox

---
+++ Ejercicio 5/Ejercicio 5/Models/PersonContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Ejercicio_5.Models
+{
+    public partial class PersonContext : DbContext
+    {
+        public PersonContext()
+        {
+        }
+
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Persona { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=EXT-PC\\SQLEXPRESS;Database=Person;persist security info=True;Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Apellido)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Carrera)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ Ejercicio 5/Ejercicio 5/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_5.Models
+{
+    public partial class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Estado { get; set; }
+        public string Carrera { get; set; }
+    }
+}
